8542b5fb7d934d221b25f1890437582ddda7c685
Umansky <umansky1@momentum15.llnl.gov>
2022-08-30 06:34:26 -0700

    Various additions and improvements

--- uedge_mvu/plot.py
+++ uedge_mvu/plot.py
@@ -15,15 +15,17 @@ from uedge import __version__ as uedgeVersion
 
 
 
-def plotmesh(iso=True, zshift=0.0, xlim=None, ylim=None, yinv=False, show=True):
+def plotmesh(iso=True, zshift=0.0, xlim=None, ylim=None, yinv=False, title="UEDGE grid", subtitle=None, show=True):
 
-    fig,ax = plt.subplots(1)
+    #fig,ax = plt.subplots(1)
 
     if (iso):
         plt.axes().set_aspect('equal', 'datalim')
     else:
         plt.axes().set_aspect('auto', 'datalim')
 
+    #plt.plot([np.min(com.rm),np.max(com.rm)], [np.min(com.zm),np.max(com.zm)])
+        
     for iy in np.arange(0,com.ny+2):
         for ix in np.arange(0,com.nx+2):
             plt.plot(com.rm[ix,iy,[1,2,4,3,1]],
@@ -32,7 +34,10 @@ def plotmesh(iso=True, zshift=0.0, xlim=None, ylim=None, yinv=False, show=True):
             
     plt.xlabel('R [m]')
     plt.ylabel('Z [m]')
-    fig.suptitle('UEDGE grid')
+    #fig.suptitle('UEDGE grid')
+    #plt.title('UEDGE grid')
+    plt.suptitle(title)
+    plt.title(subtitle, loc="left")
     plt.grid(False)
 
     if xlim:
@@ -48,7 +53,7 @@ def plotmesh(iso=True, zshift=0.0, xlim=None, ylim=None, yinv=False, show=True):
 
 
 
-def plotvar(var, zshift=0.0, iso=True, grid=False, label=None, vmin=None, vmax=None, yinv=False, title="UEDGE data"):
+def plotvar(var, zshift=0.0, iso=True, grid=False, label=None, vmin=None, vmax=None, yinv=False, title="UEDGE data", subtitle=None, show=True):
     
     patches = []
 
@@ -96,6 +101,7 @@ def plotvar(var, zshift=0.0, iso=True, grid=False, label=None, vmin=None, vmax=N
         plt.axis('equal')  # regular aspect-ratio
     
     fig.suptitle(title)
+    ax.set_title(subtitle, loc="left")
     plt.xlabel('R [m]')
     plt.ylabel('Z [m]')
 
@@ -110,12 +116,13 @@ def plotvar(var, zshift=0.0, iso=True, grid=False, label=None, vmin=None, vmax=N
     #else:
     #    plt.axes().set_aspect('auto', 'datalim')
 
-    plt.show()
+    if show:
+        plt.show()
 
 
 
 
-def plotrprof(var, ixcut=-1, title="UEDGE data", lines=True, dots=False, xlim=None, ylim=None, xlog=False, ylog=False):
+def plotrprof(var, ixcut=-1, title="UEDGE data", subtitle=None, lines=True, dots=False, xlim=None, ylim=None, xlog=False, ylog=False, show=True):
     # Plotting radial profiles of UEDGE data
     #
     # Usage example:
@@ -152,6 +159,110 @@ def plotrprof(var, ixcut=-1, title="UEDGE data", lines=True, dots=False, xlim=No
         
     plt.xlabel('R-Rsep [m]')
     fig.suptitle(title)
+    ax.set_title(subtitle, loc="right")
     plt.grid(True)
- 
+
+    if show:
+        plt.show()
+
+
+
+
+
+        
+def show_flow(vx, vy, scale=1.0, ispec=0, color="red", xlim=None, ylim=None, title=None, subtitle=None):
+#-show a vector field given by its UEDGE x,y components
+
+    #-RZ components of the vector field                                                                                
+    vr=np.zeros([com.nx+2,com.ny+2])
+    vz=np.zeros([com.nx+2,com.ny+2])
+
+
+    for ix in range(com.nx+1):
+        for jy in range(com.ny+1):
+
+	    #-unit vector in poloidal direction (given by R,Z components)                                             
+            ex=np.array([com.rm[ix,jy,2]-com.rm[ix,jy,1],com.zm[ix,jy,2]-com.zm[ix,jy,1]])
+            ex=ex/np.sqrt(np.dot(ex,ex))
+
+            #-unit vectors in the radial direction (given by R,Z components)                                          
+            ey=np.array([-ex[1],ex[0]])
+            #ey=ey/np.sqrt(np.dot(ey,ey)) #-redundant
+
+
+            #-change the direction in the private regions
+            sign=1
+            
+            ##-inner lower leg PF
+            if (ix>=0 and ix<=com.ixpt1[0] and jy<=com.iysptrx1[0]):
+                sign=-1
+                
+            ##-inner upper leg PF
+            if (ix>=com.ixpt2[0]+1 and ix<=com.ixlb[1]-1 and jy<=com.iysptrx1[0]+1):
+                sign=-1
+
+            ##-outer lower leg PF
+            if (ix>=com.ixpt2[1]+1 and ix<=com.nx+1 and jy<=com.iysptrx1[0]):
+                sign=-1
+            
+            ##-outer upper leg PF
+            if (ix>=com.ixlb[1] and ix<=com.ixpt1[1] and jy<=com.iysptrx1[0]+1):
+                sign=-1
+
+            ey=ey*sign
+                
+                            
+            #-flux vector (in RZ components)                                                                          
+            vr[ix,jy]=vx[ix,jy]*ex[0] + vy[ix,jy]*ey[0]
+            vz[ix,jy]=vx[ix,jy]*ex[1] + vy[ix,jy]*ey[1]
+
+
+            #-set zero in guard cells                                                                                 
+            if (ix==0 or ix==com.nx+1 or jy==0 or jy==com.ny+1):
+                vr[ix,jy]=0.0
+                vz[ix,jy]=0.0
+
+                
+    plotmesh(show=False, xlim=xlim, ylim=ylim, title=title, subtitle=subtitle)
+    plt.quiver(com.rm[:,:,0], com.zm[:,:,0], vr*scale, vz*scale, color=color)
     plt.show()
+
+
+
+def show_flow_ni(ispec=0, flux=False, scale=1.0, color="red", xlim=None, ylim=None, title=None, subtitle=None):
+    #-show ion or neutral density flow
+    
+    ##-X,Y components of the the flux vector
+    vx=bbb.fnix[:,:,ispec]/com.dy
+    vy=bbb.fniy[:,:,ispec]/com.dx
+
+    if not(flux):
+        #-velocity vector
+        vx=vx/bbb.ni[:,:,ispec]
+        vy=vy/bbb.ni[:,:,ispec]
+    
+    show_flow(vx, vy, scale=scale, color=color, xlim=xlim, ylim=ylim, title=title, subtitle=subtitle)
+
+
+
+    
+
+def show_flow_test(option=2, scale=1.0, color="red", xlim=None, ylim=None, title=None, subtitle=None):
+    #-show test flow
+    
+    ##-X,Y components of the the flux vector
+    
+    if (option==0):
+        print("Using unit poloidal vector flow")
+        vx=1.0+np.zeros([com.nx+2,com.ny+2])
+        vy=0.0+np.zeros([com.nx+2,com.ny+2])
+    elif (option==1):
+        print("Using unit radial vector flow")
+        vx=0.0+np.zeros([com.nx+2,com.ny+2])
+        vy=1.0+np.zeros([com.nx+2,com.ny+2])    
+    else:
+        print("Using unit radial=poloidal vector flow")
+        vx=1.0+np.zeros([com.nx+2,com.ny+2])
+        vy=1.0+np.zeros([com.nx+2,com.ny+2])
+
+    show_flow(vx, vy, scale=scale, color=color, xlim=xlim, ylim=ylim, title=title, subtitle=subtitle)
--- uedge_mvu/tstep.py
+++ uedge_mvu/tstep.py
@@ -1,4 +1,8 @@
-from uedge import bbb
+from uedge import bbb, com
+import numpy as np
+import h5py
+
+
 
 def uestep(dtnow, depth_max=10, nrefine=3, debug=False, reset=False):
 # Usage example, to integrate by deltat=1.0 s, do:
@@ -49,3 +53,59 @@ def uestep(dtnow, depth_max=10, nrefine=3, debug=False, reset=False):
 uestep.abort=False
 uestep.depth=0
 uestep.deltat=0.0
+
+
+
+
+def run_time_evol(dt=1e0, nt=1, save=False, filename="thist.h5"):
+#-run a sequence of time steps of the same size
+
+    ni_hist = np.zeros([com.nx+2, com.ny+2, 2, nt+1])
+    up_hist = np.zeros([com.nx+2, com.ny+2, 2, nt+1])
+    te_hist = np.zeros([com.nx+2, com.ny+2, nt+1])
+    ti_hist = np.zeros([com.nx+2, com.ny+2, nt+1])
+
+    ni_hist[:,:,:,0]=bbb.ni
+    up_hist[:,:,:,0]=bbb.up
+    te_hist[:,:,0]=bbb.te
+    ti_hist[:,:,0]=bbb.ti
+
+
+    for it in range(1,nt+1):
+        print("it=", it)
+        uestep(dt)
+        ni_hist[:,:,:,it]=bbb.ni
+        up_hist[:,:,:,it]=bbb.up
+        te_hist[:,:,it]=bbb.te
+        ti_hist[:,:,it]=bbb.ti
+
+    
+    if (save):
+        print("Saving in ", filename)
+        
+        with h5py.File(filename, 'w') as f:
+            f['ni_hist'] = ni_hist
+            f['up_hist'] = up_hist
+            f['te_hist'] = te_hist
+            f['ti_hist'] = ti_hist
+
+
+
+            
+def restore_time_evol(filename="thist.h5"):
+#-restore data from time evolution file
+    
+    with h5py.File(filename, 'r') as f:
+        dataset = f.get('ni_hist')  # get the h5py.Dataset
+        ni_hist = dataset[:]  # Copy the array into memory
+
+        dataset = f.get('up_hist')  # get the h5py.Dataset
+        up_hist = dataset[:]  # Copy the array into memory
+
+        dataset = f.get('te_hist')  # get the h5py.Dataset
+        te_hist = dataset[:]  # Copy the array into memory
+
+        dataset = f.get('ti_hist')  # get the h5py.Dataset
+        ti_hist = dataset[:]  # Copy the array into memory
+
+    return ni_hist, up_hist, te_hist, ti_hist
